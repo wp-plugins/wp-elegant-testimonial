494fc3fab7bbca2b4794b98787d2081de34d8ae8
wpreviewproduct <wpreviewproduct@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-06 02:23:56 +0000

    First Version
    git-svn-id: https://plugins.svn.wordpress.org/wp-elegant-testimonial/trunk@568102 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== WP Elegant Testimonial ===
+Contributors: WPREVIEWPRODUCT
+Donate link: http://wpreviewproduct.com/
+Tags: wordpress testimonial, wordpress testimonial plugin, wp testimonial
+Requires at least: 3.0
+Tested up to: 3.4.1
+Stable tag: 1.1
+License: GPLv2 or later
+
+An easy to use Testimonial Plugin for WordPress.
+
+== Description ==
+
+WP Elegant Testimonial is an easy to use WordPress Plugin to insert testimonials to a post or page. There is no limit to the number of testimonials you can use on a specific page. You can insert as many testimonials as you want using shortcodes.
+
+For detailed documentation please visit: [WordPress Elegant Testimonial Plugin Page](http://wpreviewproduct.com/?p=229)
+
+== Usage ==
+
+You need to embed the appropriate shortcode to add testimonial to a WordPress post or page. Once you have installed the plugin please go to "WP Elegant Testimonial" option under "Settings". Follw the instructions for using the shortcode.
+
+For more info please refer to the plugin's homepage:
+[WP Elegant Testimonial Usage](http://wpreviewproduct.com/?p=229)
+
+== Installation ==
+
+Upload the plugin to the plugins directory via WordPress Plugin Uploader (Plugins->Add New->Upload->Choose File->Install Now) and Activate it.
+
+== Changelog ==
+
+=1.1=
+* First release
+
+== Frequently Asked Questions ==
+
+= Can this plugin be used to embed testimonials? =
+Yes
+
+== Screenshots ==
+
+None
+
+== Upgrade Notice ==
+
+
---
+++ wp-elegant-testimonial.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: WP Elegant Testimonial
+Version: 1.1
+Plugin URI: http://wpreviewproduct.com/?p=229
+Author: WPREVIEWPRODUCT
+Author URI: http://wpreviewproduct.com/
+Description: A simple WordPress plugin to add testimonials to a WordPress post or page.
+*/
+define('WP_ELEGANT_TESTIMONIAL_URL', plugins_url('',__FILE__));
+define('WP_ELEGANT_TESTIMONIAL_VERSION', "1.1");
+include_once('wp-elegant-testimonial-menu.php');
+add_action('wp_enqueue_scripts', 'wp_elegant_testimonial_enqueue_scripts');
+add_action( 'admin_menu', 'wp_elegant_testimonial_plugin_menu' );
+add_shortcode('wp_elegant_testimonial','wp_elegant_testimonial_handler');
+
+function wp_elegant_testimonial_plugin_menu()
+{
+	add_options_page('WP Elegant Testimonial Options', 'WP Elegant Testimonial', 'manage_options', 'wp-elegant-testimonial', 'wp_elegant_testimonial_display_menu' );	
+}
+
+function wp_elegant_testimonial_handler($atts)
+{
+	extract(shortcode_atts(array(
+		'name' => '',
+		'company' => '',
+		'text' => '',
+		'image' => '',
+	), $atts));
+	$div_id = "wp-elegant-testimonial".uniqid();
+	$output = <<<EOT
+    <div id="$div_id" style="width: 200px;padding: 5px;">
+    	<div style="text-align: center;">
+	        <div class="theme-left"><img src="$image" title="$text"/></div>
+	        <div class="testimonials-title">$name</div>
+	        <div class="testimonials-company">$company</div>
+        </div>
+    </div>
+	<script type="text/javascript">
+	/* <![CDATA[ */
+	jQuery(document).ready(function($){
+	      $("#$div_id img[title]").tooltip();
+		});
+	/*]]>*/
+	</script>
+EOT;
+	return $output; 
+}
+
+function wp_elegant_testimonial_enqueue_scripts()
+{
+	if (!is_admin()) 
+    {
+    	wp_enqueue_script('jquery');
+    	wp_register_script('jquery-tools', 'http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js');
+	    wp_enqueue_script('jquery-tools');
+    	wp_register_style('wp-elegant-testimonial-style', WP_ELEGANT_TESTIMONIAL_URL.'/wp-elegant-testimonial.css');
+        wp_enqueue_style('wp-elegant-testimonial-style');	
+    }	
+}
+
+?>
